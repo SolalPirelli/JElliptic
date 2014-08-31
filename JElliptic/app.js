2f6b01fd03615cdca80da6a9e7acbec769424dce
Solal Pirelli <solal.pirelli@gmail.com>
2014-08-31 17:38:10 +0200

    Use elliptic curves (doesn't work yet)

--- JElliptic/app.js
+++ JElliptic/app.js
@@ -1,12 +1,12 @@
 ﻿// Logs debugging messages
-var Debug = (function () {
-    function Debug() {
-    }
-    Debug.clear = function () {
+var Debug;
+(function (Debug) {
+    function clear() {
         document.getElementById("log").innerHTML = "";
-    };
+    }
+    Debug.clear = clear;
 
-    Debug.log = function (str) {
+    function log(str) {
         var elem = document.getElementById("log");
 
         var span = document.createElement("span");
@@ -16,119 +16,266 @@ var Debug = (function () {
 
         elem.appendChild(span);
         elem.appendChild(br);
-    };
-    return Debug;
-})();
-
-// Partition for elements of a group.
-var Partition;
-(function (Partition) {
-    Partition[Partition["P0"] = 0] = "P0";
-    Partition[Partition["P1"] = 1] = "P1";
-    Partition[Partition["P2"] = 2] = "P2";
-})(Partition || (Partition = {}));
-
-// Element of a group G.
-var G = (function () {
-    // Creates a new element from the specified number.
-    function G(value) {
-        this.value = value;
     }
-    G.prototype.partition = function () {
-        switch (this.value % 3) {
-            case 0:
-                return 0 /* P0 */;
-            case 1:
-                return 1 /* P1 */;
-            case 2:
-                return 2 /* P2 */;
+    Debug.log = log;
+})(Debug || (Debug = {}));
+
+var ModMath;
+(function (ModMath) {
+    // Non-negative mod
+    function mod(a, n) {
+        return ((a % n) + n) % n;
+    }
+    ModMath.mod = mod;
+
+    // from http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Modular_integers
+    function modInverse(a, n) {
+        var t = 0, newt = 1;
+        var r = n, newr = a;
+        while (newr != 0) {
+            var quotient = Math.floor(r / newr);
+
+            var oldt = t;
+            t = newt;
+            newt = oldt - quotient * newt;
+
+            var oldr = r;
+            r = newr;
+            newr = oldr - quotient * newr;
         }
+        if (r > 1) {
+            alert(a + " is not invertible");
+        }
+        if (t < 0) {
+            t = t + n;
+        }
+        return t;
+    }
+    ModMath.modInverse = modInverse;
+})(ModMath || (ModMath = {}));
+
+var ModNum = (function () {
+    function ModNum(val, n) {
+        this.val = ModMath.mod(val, n);
+        this.n = n;
+    }
+    ModNum.prototype.negate = function () {
+        return new ModNum(-this.value, this.n);
     };
 
-    G.prototype.mul = function (other) {
-        return new G((this.value * other.value) % (G.Order + 1));
+    ModNum.prototype.add = function (other) {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
+        return new ModNum(this.val + other.val, this.n);
     };
 
-    G.prototype.eq = function (other) {
+    ModNum.prototype.addNum = function (n) {
+        return new ModNum(this.val + n, this.n);
+    };
+
+    ModNum.prototype.sub = function (other) {
+        return this.add(other.negate());
+    };
+
+    ModNum.prototype.mul = function (other) {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
+        return new ModNum(this.val * other.val, this.n);
+    };
+
+    ModNum.prototype.mulNum = function (n) {
+        return new ModNum(n * this.val, this.n);
+    };
+
+    ModNum.prototype.div = function (other) {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
+        return new ModNum(this.val * ModMath.modInverse(other.val, this.n), this.n);
+    };
+
+    ModNum.prototype.pow = function (n) {
+        var result = this;
+        for (var _ = 1; _ < n; _++) {
+            result = result.mul(this);
+        }
+        return result;
+    };
+
+    ModNum.prototype.eq = function (other) {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
         return this.value == other.value;
     };
 
-    // Override toString for debugging
-    G.prototype.toString = function () {
-        return "[G: " + this.value + "]";
+    ModNum.prototype.value = function () {
+        return this.val;
     };
-    G.One = new G(1);
-    return G;
+
+    // override toString for debugging
+    ModNum.prototype.toString = function () {
+        return "[" + this.value + " mod " + this.n + "]";
+    };
+    return ModNum;
+})();
+
+// Point in an elliptic curve
+var ModPoint = (function () {
+    function ModPoint() {
+    }
+    // Creates a new element from the specified number.
+    ModPoint.create = function (x, y, a, b, n) {
+        return ModPoint.createNum(new ModNum(x, n), new ModNum(y, n), new ModNum(a, n), new ModNum(b, n), n);
+    };
+
+    ModPoint.createNum = function (x, y, a, b, n) {
+        var p = new ModPoint();
+        p.x = x;
+        p.y = y;
+        p.a = a;
+        p.b = b;
+
+        if (!y.pow(2).eq(x.pow(3).add(a.mul(p.x)).add(b))) {
+            Debug.log("!!! ERROR: " + p + " is not a valid point !!!");
+        }
+
+        return p;
+    };
+
+    // Adds this element with another
+    ModPoint.prototype.add = function (other) {
+        var lambda;
+        if (this.eq(other)) {
+            // Double
+            var num = this.x.pow(2).mulNum(3).add(this.a);
+            var denom = this.y.mulNum(2);
+            lambda = num.div(denom);
+        } else {
+            // Add
+            var num = other.y.sub(this.y);
+            var denom = other.x.sub(this.x);
+            lambda = num.div(denom);
+        }
+
+        var x = lambda.pow(2).sub(this.x).sub(other.x);
+        var y = lambda.mul(this.x.sub(x)).sub(this.y);
+
+        return ModPoint.createNum(x, y, this.a, this.b, this.n);
+    };
+
+    ModPoint.prototype.mulNum = function (c) {
+        var g = this;
+        for (var n = 1; n < c; n++) {
+            g = g.add(this);
+        }
+
+        return g;
+    };
+
+    ModPoint.prototype.partition = function (count) {
+        return this.x.value() % count;
+    };
+
+    ModPoint.prototype.eq = function (other) {
+        return this.x.eq(other.x) && this.y.eq(other.y);
+    };
+
+    // Override toString for debugging
+    ModPoint.prototype.toString = function () {
+        return "[" + this.x.value() + ", " + this.y.value() + "] % " + this.n + "(A = " + this.a.value() + ", B = " + this.b.value() + ")";
+    };
+    return ModPoint;
 })();
 
 // Walk over G
 var Walk = (function () {
-    function Walk(alpha, beta) {
-        this.alpha = alpha;
-        this.beta = beta;
-        this.a = 0;
-        this.b = 0;
-        this.x = G.One;
+    function Walk() {
     }
+    Walk.create = function (gx, gy, hx, hy, a, b, n) {
+        var w = new Walk();
+        w.g = ModPoint.create(gx, gy, a, b, n);
+        w.h = ModPoint.create(hx, hy, a, b, n);
+        w.u = new ModNum(1, n);
+        w.v = new ModNum(0, n);
+        w.p = w.g;
+        return w;
+    };
+
+    Walk.createNum = function (g, h, u, v) {
+        var w = new Walk();
+        w.g = g;
+        w.h = h;
+        w.u = u;
+        w.v = v;
+        w.p = g.mulNum(u.value()).add(h.mulNum(v.value()));
+        return w;
+    };
+
     Walk.prototype.step = function () {
-        switch (this.x.partition()) {
-            case 0 /* P0 */:
-                this.a = (2 * this.a) % G.Order;
-                this.b = (2 * this.b) % G.Order;
-                this.x = this.x.mul(this.x);
-                return;
+        var u, v;
+        switch (this.p.partition(3)) {
+            case 0:
+                u = this.u.mulNum(2);
+                v = this.v.mulNum(2);
+                break;
 
-            case 1 /* P1 */:
-                this.a = (this.a + 1) % G.Order;
-                this.x = this.alpha.mul(this.x);
-                return;
+            case 1:
+                u = this.u.addNum(1);
+                v = this.v;
+                break;
 
-            case 2 /* P2 */:
-                this.b = (this.b + 1) % G.Order;
-                this.x = this.beta.mul(this.x);
-                return;
+            case 2:
+                u = this.u;
+                v = this.v.addNum(1);
+                break;
         }
+
+        return Walk.createNum(this.g, this.h, u, v);
+    };
+
+    Walk.prototype.eq = function (other) {
+        return this.v == other.v && this.u == other.u;
     };
 
     // Override toString for debugging
     Walk.prototype.toString = function () {
-        return "[Walk: a = " + this.a + ", b = " + this.b + ", x = " + this.x + "]";
+        return "[Walk: u = " + this.u + ", v = " + this.v + ", p = " + this.p + "]";
     };
     return Walk;
 })();
 
 // Simple Pollard's Rho for logarithms implementation
-var PollardRho = (function () {
-    function PollardRho() {
-    }
-    PollardRho.solve = function (alpha, beta) {
-        var tortoise = new Walk(alpha, beta);
-        var hare = new Walk(alpha, beta);
+var PollardRho;
+(function (PollardRho) {
+    // Find m such that m*g = h
+    function solve(gx, gy, hx, hy, a, b, n) {
+        var tortoise = Walk.create(gx, gy, hx, hy, a, b, n);
+        var hare = tortoise;
 
-        for (var step = 1; step < G.Order; step++) {
-            tortoise.step();
+        for (var step = 1; step < n; step++) {
+            tortoise = tortoise.step();
 
-            hare.step();
-            hare.step();
+            hare = hare.step().step();
 
             Debug.log("Step: " + step);
             Debug.log("Tortoise: " + tortoise);
             Debug.log("Hare: " + hare);
             Debug.log("---");
 
-            if (tortoise.x.eq(hare.x)) {
-                var r = tortoise.b - hare.b;
-
-                if (r == 0) {
-                    return null;
-                }
-
-                return (1.0 / r * (hare.a - tortoise.a)) % G.Order;
+            if (tortoise.p.eq(hare.p) && !tortoise.eq(hare)) {
+                return tortoise.u.sub(hare.u).div(hare.v.sub(tortoise.v)).value();
             }
         }
-    };
-    return PollardRho;
-})();
+    }
+    PollardRho.solve = solve;
+})(PollardRho || (PollardRho = {}));
 
 // Gets the value of the input with the specified name, as a number
 function numberValue(elemName) {
@@ -137,17 +284,17 @@ function numberValue(elemName) {
 
 window.onload = function () {
     var btn = document.getElementById("button");
-    var el = document.getElementById("content");
+    var content = document.getElementById("content");
 
     btn.onclick = function (_) {
-        var alpha = numberValue("alpha");
-        var beta = numberValue("beta");
-        var order = numberValue("order");
+        var a = numberValue("a"), b = numberValue("b"), n = numberValue("order");
+        var gx = numberValue("gx"), gy = numberValue("gy");
+        var hx = numberValue("hx"), hy = numberValue("hy");
 
         Debug.clear();
 
-        G.Order = order;
-        el.textContent = PollardRho.solve(new G(alpha), new G(beta)).toString();
+        var m = PollardRho.solve(gx, gy, hx, hy, a, b, n);
+        content.textContent = (m || "Error").toString();
     };
 };
 //# sourceMappingURL=app.js.map
--- JElliptic/app.ts
+++ JElliptic/app.ts
@@ -1,10 +1,10 @@
 ﻿// Logs debugging messages
-class Debug {
-    static clear() {
+module Debug {
+    export function clear() {
         document.getElementById("log").innerHTML = "";
     }
 
-    static log(str: string) {
+    export function log(str: string) {
         var elem = document.getElementById("log");
 
         var span = document.createElement("span");
@@ -17,122 +17,271 @@ class Debug {
     }
 }
 
-// Partition for elements of a group.
-enum Partition {
-    P0, P1, P2
+module ModMath {
+    // Non-negative mod
+    export function mod(a: number, n: number): number {
+        return ((a % n) + n) % n;
+    }
+
+    // from http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Modular_integers
+    export function modInverse(a: number, n: number): number {
+        var t = 0, newt = 1;
+        var r = n, newr = a;
+        while (newr != 0) {
+            var quotient = Math.floor(r / newr);
+
+            var oldt = t;
+            t = newt;
+            newt = oldt - quotient * newt;
+
+            var oldr = r;
+            r = newr;
+            newr = oldr - quotient * newr;
+        }
+        if (r > 1) {
+            alert(a + " is not invertible");
+        }
+        if (t < 0) {
+            t = t + n;
+        }
+        return t;
+    }
 }
 
-// Element of a group G.
-class G {
-    // Implementation detail!
-    private value: number;
+class ModNum {
+    private val: number;
+    private n: number;
+
+    constructor(val: number, n: number) {
+        this.val = ModMath.mod(val, n);
+        this.n = n;
+    }
+
+    negate(): ModNum {
+        return new ModNum(-this.value, this.n);
+    }
+
+    add(other: ModNum): ModNum {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
+        return new ModNum(this.val + other.val, this.n);
+    }
+
+    addNum(n: number): ModNum {
+        return new ModNum(this.val + n, this.n);
+    }
+
+    sub(other: ModNum): ModNum {
+        return this.add(other.negate());
+    }
+
+    mul(other: ModNum): ModNum {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
+        return new ModNum(this.val * other.val, this.n);
+    }
+
+    mulNum(n: number): ModNum {
+        return new ModNum(n * this.val, this.n);
+    }
+
+    div(other: ModNum): ModNum {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
+        return new ModNum(this.val * ModMath.modInverse(other.val, this.n), this.n);
+    }
+
+    pow(n: number): ModNum {
+        var result = this;
+        for (var _ = 1; _ < n; _++) {
+            result = result.mul(this);
+        }
+        return result;
+    }
+
+    eq(other: ModNum): boolean {
+        if (this.n != other.n) {
+            throw "Incompatible ModNums";
+        }
+
+        return this.value == other.value;
+    }
+
+    value(): number {
+        return this.val;
+    }
+
+    // override toString for debugging
+    toString(): string {
+        return "[" + this.value + " mod " + this.n + "]";
+    }
+}
+
+// Point in an elliptic curve
+class ModPoint {
+    // y^2 = x^3 + ax + b (mod n)
+    private x: ModNum;
+    private y: ModNum;
+    private a: ModNum;
+    private b: ModNum;
+    private n: number;
 
     // Creates a new element from the specified number.
-    constructor(value: number) {
-        this.value = value;
+    public static create(x: number, y: number, a: number, b: number, n: number) {
+        return ModPoint.createNum(new ModNum(x, n), new ModNum(y, n), new ModNum(a, n), new ModNum(b, n), n);
     }
 
+    private static createNum(x: ModNum, y: ModNum, a: ModNum, b: ModNum, n: number) {
+        var p = new ModPoint();
+        p.x = x;
+        p.y = y;
+        p.a = a;
+        p.b = b;
 
-    // Gets the multiplication-neutral element.
-    static One = new G(1);
-    // Gets (or sets) the order of the group.
-    static Order;
-
-
-    partition() {
-        switch (this.value % 3) {
-            case 0:
-                return Partition.P0;
-            case 1:
-                return Partition.P1;
-            case 2:
-                return Partition.P2;
+        if (!y.pow(2).eq(x.pow(3).add(a.mul(p.x)).add(b))) {
+            Debug.log("!!! ERROR: " + p + " is not a valid point !!!");
         }
+
+        return p;
     }
 
-    mul(other: G): G {
-        return new G((this.value * other.value) % (G.Order + 1));
+
+    // Adds this element with another
+    add(other: ModPoint): ModPoint {
+        var lambda: ModNum;
+        if (this.eq(other)) {
+            // Double
+            var num = this.x.pow(2).mulNum(3).add(this.a);
+            var denom = this.y.mulNum(2);
+            lambda = num.div(denom);
+        } else {
+            // Add
+            var num = other.y.sub(this.y);
+            var denom = other.x.sub(this.x);
+            lambda = num.div(denom);
+        }
+
+        var x = lambda.pow(2).sub(this.x).sub(other.x);
+        var y = lambda.mul(this.x.sub(x)).sub(this.y);
+
+        return ModPoint.createNum(x, y, this.a, this.b, this.n);
     }
 
-    eq(other: G): boolean {
-        return this.value == other.value;
+    mulNum(c: number): ModPoint {
+        var g = this;
+        for (var n = 1; n < c; n++) {
+            g = g.add(this);
+        }
+
+        return g;
+    }
+
+    partition(count: number): number {
+        return this.x.value() % count;
+    }
+
+    eq(other: ModPoint): boolean {
+        return this.x.eq(other.x) && this.y.eq(other.y);
     }
 
     // Override toString for debugging
     toString(): string {
-        return "[G: " + this.value + "]";
+        return "[" + this.x.value() + ", " + this.y.value() + "] % " + this.n + "(A = " + this.a.value() + ", B = " + this.b.value() + ")";
     }
 }
 
 // Walk over G
 class Walk {
-    alpha: G;
-    beta: G;
-    a: number;
-    b: number;
-    x: G;
+    // we want to find m such that m*g = h
+    private g: ModPoint;
+    private h: ModPoint;
+
+    // random multipliers
+    public u: ModNum;
+    public v: ModNum;
+
+    // current value
+    public p: ModPoint;
 
 
-    constructor(alpha, beta) {
-        this.alpha = alpha;
-        this.beta = beta;
-        this.a = 0;
-        this.b = 0;
-        this.x = G.One;
+    public static create(gx: number, gy: number, hx: number, hy: number, a: number, b: number, n: number): Walk {
+        var w = new Walk();
+        w.g = ModPoint.create(gx, gy, a, b, n);
+        w.h = ModPoint.create(hx, hy, a, b, n);
+        w.u = new ModNum(1, n);
+        w.v = new ModNum(0, n);
+        w.p = w.g;
+        return w;
+    }
+
+    private static createNum(g: ModPoint, h: ModPoint, u: ModNum, v: ModNum): Walk {
+        var w = new Walk();
+        w.g = g;
+        w.h = h;
+        w.u = u;
+        w.v = v;
+        w.p = g.mulNum(u.value()).add(h.mulNum(v.value()));
+        return w;
     }
 
 
-    step(): void {
-        switch (this.x.partition()) {
-            case Partition.P0:
-                this.a = (2 * this.a) % G.Order;
-                this.b = (2 * this.b) % G.Order;
-                this.x = this.x.mul(this.x);
-                return;
+    step(): Walk {
+        var u: ModNum, v: ModNum;
+        switch (this.p.partition(3)) {
+            case 0:
+                u = this.u.mulNum(2);
+                v = this.v.mulNum(2);
+                break;
 
-            case Partition.P1:
-                this.a = (this.a + 1) % G.Order;
-                this.x = this.alpha.mul(this.x);
-                return;
+            case 1:
+                u = this.u.addNum(1);
+                v = this.v;
+                break;
 
-            case Partition.P2:
-                this.b = (this.b + 1) % G.Order;
-                this.x = this.beta.mul(this.x);
-                return;
+            case 2:
+                u = this.u;
+                v = this.v.addNum(1);
+                break;
         }
+
+        return Walk.createNum(this.g, this.h, u, v);
+    }
+
+    eq(other: Walk): boolean {
+        return this.v == other.v && this.u == other.u;
     }
 
     // Override toString for debugging
     toString(): string {
-        return "[Walk: a = " + this.a + ", b = " + this.b + ", x = " + this.x + "]";
+        return "[Walk: u = " + this.u + ", v = " + this.v + ", p = " + this.p + "]";
     }
 }
 
 // Simple Pollard's Rho for logarithms implementation
-class PollardRho {
-    static solve(alpha: G, beta: G): number {
-        var tortoise = new Walk(alpha, beta);
-        var hare = new Walk(alpha, beta);
+module PollardRho {
+    // Find m such that m*g = h
+    export function solve(gx: number, gy: number, hx: number, hy: number, a: number, b: number, n: number): number {
+        var tortoise = Walk.create(gx, gy, hx, hy, a, b, n);
+        var hare = tortoise;
 
-        for (var step = 1; step < G.Order; step++) {
-            tortoise.step();
+        for (var step = 1; step < n; step++) {
+            tortoise = tortoise.step();
 
-            hare.step();
-            hare.step();
+            hare = hare.step().step();
 
             Debug.log("Step: " + step);
             Debug.log("Tortoise: " + tortoise);
             Debug.log("Hare: " + hare);
             Debug.log("---");
 
-            if (tortoise.x.eq(hare.x)) {
-                var r = tortoise.b - hare.b;
-
-                if (r == 0) {
-                    return null;
-                }
-
-                return (1.0 / r * (hare.a - tortoise.a)) % G.Order;
+            if (tortoise.p.eq(hare.p) && !tortoise.eq(hare)) {
+                return tortoise.u.sub(hare.u).div(hare.v.sub(tortoise.v)).value();
             }
         }
     }
@@ -145,16 +294,16 @@ function numberValue(elemName: string): number {
 
 window.onload = () => {
     var btn = document.getElementById("button");
-    var el = document.getElementById("content");
+    var content = document.getElementById("content");
 
     btn.onclick = _ => {
-        var alpha = numberValue("alpha");
-        var beta = numberValue("beta");
-        var order = numberValue("order");
+        var a = numberValue("a"), b = numberValue("b"), n = numberValue("order");
+        var gx = numberValue("gx"), gy = numberValue("gy");
+        var hx = numberValue("hx"), hy = numberValue("hy");
 
         Debug.clear();
 
-        G.Order = order;
-        el.textContent = PollardRho.solve(new G(alpha), new G(beta)).toString();
+        var m = PollardRho.solve(gx, gy, hx, hy, a, b, n);
+        content.textContent = (m || "Error").toString();
     };
 };
