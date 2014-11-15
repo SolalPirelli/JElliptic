8491ef7528ed0e8dd3ff2fd531d043b8427da672
Solal Pirelli <solal.pirelli@gmail.com>
2014-11-15 16:17:51 +0100

    Added benchmarks.

---
+++ JElliptic/bench/bench.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="lib/benchmark.d.ts" />
+
+import BigInteger = require("../BigInteger");
+
+var resultsContainer = <HTMLTableElement> document.getElementById("results");
+var infoTextContainer = <HTMLSpanElement>document.getElementById("infoText");
+
+function addResult(result: string): void {
+    var row = <HTMLTableRowElement> resultsContainer.insertRow();
+    var cell = <HTMLTableCellElement> row.insertCell();
+    var text = document.createTextNode(result);
+    cell.appendChild(text);
+}
+
+function setInfoText(text: string): void {
+    infoTextContainer.textContent = text;
+}
+
+requirejs([], () => {
+    setInfoText("Running tests...");
+
+    new Benchmark.Suite()
+        .add("BigInteger: 1 + 1", () => {
+            BigInteger.ONE.add(BigInteger.ONE);
+        })
+        .on("cycle", evt => {
+            addResult(evt.target.toString());
+        })
+        .on("complete", () => {
+            setInfoText("Finished");
+        })
+        .run({ "async": true });
+});
---
+++ JElliptic/bench/lib/benchmark.d.ts
@@ -0,0 +1,27 @@
+﻿// Very tiny definition file extracted from http://benchmarkjs.com/docs
+
+declare module Benchmark {
+    class Suite {
+        /** Creates a new Suite. */
+        constructor();
+
+        /** Adds a test to the suite. */
+        add(name: string, func: () => void): Suite;
+
+        /** Adds a listener to a suite event. */
+        on(name: string, callback: (obj: any) => void): Suite;
+
+        /** Adds a listener called each time a benchmark is finished. */
+        on(name: "cycle", callback: (evt: Event) => void): Suite;
+
+        /** Adds a listener called when the suite has finished running. */
+        on(name: "complete", callback: () => void): Suite;
+
+        /** Runs the suite. */
+        run(options?: any): void;
+    }
+
+    class Event {
+        target: any;
+    }
+}
