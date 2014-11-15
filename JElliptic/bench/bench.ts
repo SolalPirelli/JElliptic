cdb7e2695c563a7812ab74b90594b3dc6c8ceea3
Solal Pirelli <solal.pirelli@gmail.com>
2014-11-15 23:37:30 +0100

    Finished benchmarks for BigInteger.

--- JElliptic/bench/bench.ts
+++ JElliptic/bench/bench.ts
@@ -42,13 +42,14 @@ function createSuite(suiteName: string) {
 }
 
 
-var i1 = BigInteger.parse("1");
+var i1_1 = BigInteger.parse("1");
+var i1_2 = BigInteger.parse("9");
 
 var i20_1 = BigInteger.parse("12345678901234567890");
 var i20_2 = BigInteger.parse("99999999999999999999");
 
-var i100_1 = BigInteger.parse("9876543210987654321098765432109876543210987654321098765432109876543210987654321098765432109876543210");
-var i100_2 = BigInteger.parse("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+var i100_1 = BigInteger.parse("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+var i100_2 = BigInteger.parse("9876543210987654321098765432109876543210987654321098765432109876543210987654321098765432109876543210");
 
 requirejs([], () => {
     var bi = createSuite("BigInteger");
@@ -60,28 +61,49 @@ requirejs([], () => {
     bi.add("Parse 20-digit number", () => BigInteger.parse("1234578901234567890"));
     bi.add("Parse 100-digit number", () => BigInteger.parse("12345789012345678901234578901234567890123457890123456789012345789012345678901234578901234567890"));
 
-    bi.add("Negate a 1-digit number", () => i1.negate());
+    bi.add("Negate a 1-digit number", () => i1_1.negate());
     bi.add("Negate a 20-digit number", () => i20_1.negate());
     bi.add("Negate a 100-digit number", () => i100_1.negate());
 
-    bi.add("Absolute value of a 1-digit number", () => i1.abs());
+    bi.add("Absolute value of a 1-digit number", () => i1_1.abs());
     bi.add("Absolute value of a 20-digit number", () => i20_1.abs());
     bi.add("Absolute value of a 100-digit number", () => i100_1.abs());
 
-    bi.add("Add two 1-digit numbers", () => i1.add(i1));
-    bi.add("Add a 20-digit number and 1", () => i20_1.add(i1));
+    bi.add("Add two 1-digit numbers", () => i1_1.add(i1_2));
+    bi.add("Add 20-digit and 1-digit numbers", () => i20_1.add(i1_1));
     bi.add("Add two 20-digit numbers", () => i20_1.add(i20_2));
     bi.add("Add two 100-digit numbers", () => i100_1.add(i100_2));
 
-    bi.add("Subtract two 1-digit numbers", () => i1.sub(i1));
-    bi.add("Subtract a 20-digit number and 1", () => i20_1.sub(i1));
+    bi.add("Subtract two 1-digit numbers", () => i1_1.sub(i1_2));
+    bi.add("Subtract 20-digit and 1-digit numbers", () => i20_1.sub(i1_1));
     bi.add("Subtract two 20-digit numbers", () => i20_1.sub(i20_2));
     bi.add("Subtract two 100-digit numbers", () => i100_1.sub(i100_2));
 
-    bi.add("Multiply two 1-digit numbers", () => i1.mul(i1));
-    bi.add("Multiply a 20-digit number and 1", () => i20_1.mul(i1));
+    bi.add("Multiply two 1-digit numbers", () => i1_1.mul(i1_2));
+    bi.add("Multiply 20-digit and 1-digit numbers", () => i20_1.mul(i1_1));
     bi.add("Multiply two 20-digit numbers", () => i20_1.mul(i20_2));
     bi.add("Multiply two 100-digit numbers", () => i100_1.mul(i100_2));
+
+    // FIXME: div, mod and modInverse are so incredibly slow they're not worth benchmarking :-/
+    //bi.add("Divide two 1-digit numbers", () => i1_1.div(i1_2));
+    //bi.add("Divide 20-digit and 1-digit numbers", () => i20_1.div(i1_1));
+    //bi.add("Divide two 20-digit numbers", () => i20_1.div(i20_2));
+    //bi.add("Divide two 100-digit numbers", () => i100_1.div(i100_2));
+
+    //bi.add("Modulo two 1-digit numbers", () => i1_1.mod(i1_2));
+    //bi.add("Modulo 20-digit and 1-digit numbers", () => i20_1.mod(i1_1));
+    //bi.add("Modulo two 20-digit numbers", () => i20_1.mod(i20_2));
+    //bi.add("Modulo two 100-digit numbers", () => i100_1.mod(i100_2));
+
+    //bi.add("Modular inverse of a 1-digit number", () => i1_1.modInverse(i1_2));
+    //bi.add("Modular inverse a 20-digit number", () => i20_1.modInverse(i20_2));
+    //bi.add("Modular inverse a 100-digit number", () => i100_1.modInverse(i100_2));
+
+    bi.add("Logical AND of two 1-digit numbers", () => i1_1.and(i1_2));
+    bi.add("Logical AND of 20-digit and 1-digit numbers", () => i20_1.and(i1_1));
+    bi.add("Logical AND of two 20-digit numbers", () => i20_1.and(i20_2));
+    bi.add("Logical AND of 100-digit and 20-digit numbers", () => i100_1.and(i20_1));
+    bi.add("Logical AND of two 100-digit numbers", () => i100_1.and(i100_2));
 
     bi.run({ async: true });
 });
