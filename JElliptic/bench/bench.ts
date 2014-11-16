87bb8b3e5bc02942d7bfc1ba4cb619de8da762d0
Solal Pirelli <solal.pirelli@gmail.com>
2014-11-16 13:45:23 +0100

    Benchmark can now show multiple suites.

--- JElliptic/bench/bench.ts
+++ JElliptic/bench/bench.ts
@@ -2,8 +2,17 @@
 
 import BigInteger = require("../BigInteger");
 
-function createSuite(suiteName: string) {
+var SUITE_PANE_WIDTH = 400;
+var suites = new Array<Benchmark.Suite>();
+var currentSuiteIndex = 0;
+function createSuite(suiteName: string): Benchmark.Suite {
     var container = document.createElement("div");
+    // HACK: CSS sucks.
+    var suitesCount = suites.length;
+    container.style.position = "absolute";
+    container.style.left = suitesCount * SUITE_PANE_WIDTH + "px";
+    container.style.top = "0px";
+    container.style.maxWidth = SUITE_PANE_WIDTH + "px";
     document.body.appendChild(container);
 
     var header = document.createElement("b");
@@ -34,55 +43,70 @@ function createSuite(suiteName: string) {
         container.appendChild(document.createElement("br"));
     }
 
-    return new Benchmark.Suite(suiteName, {
+    setInfoText("Waiting...");
+
+    var suite = new Benchmark.Suite(suiteName, {
         onStart: () => setInfoText("Running benchmarks..."),
         onCycle: evt => addResult(evt),
-        onComplete: () => setInfoText("Finished.")
+        onComplete: () => {
+            setInfoText("Finished.");
+            currentSuiteIndex++;
+            if (suites.length > currentSuiteIndex) {
+                suites[currentSuiteIndex].run({ async: true });
+            }
+        }
     });
+
+    suites.push(suite);
+
+    return suite;
 }
 
+function run(...suites: Benchmark.Suite[]): void {
+    suites[0].run({ async: true });
+}
 
-var i1_1 = BigInteger.parse("1");
-var i1_2 = BigInteger.parse("9");
+function bigIntegerSuite(): Benchmark.Suite {
+    var i1_1 = BigInteger.parse("1");
+    var i1_2 = BigInteger.parse("9");
 
-var i20_1 = BigInteger.parse("12345678901234567890");
-var i20_2 = BigInteger.parse("99999999999999999999");
+    var i20_1 = BigInteger.parse("12345678901234567890");
+    var i20_2 = BigInteger.parse("99999999999999999999");
 
-var i100_1 = BigInteger.parse("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
-var i100_2 = BigInteger.parse("9876543210987654321098765432109876543210987654321098765432109876543210987654321098765432109876543210");
+    var i100_1 = BigInteger.parse("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+    var i100_2 = BigInteger.parse("9876543210987654321098765432109876543210987654321098765432109876543210987654321098765432109876543210");
 
-requirejs([], () => {
-    var bi = createSuite("BigInteger");
+    var s = createSuite("BigInteger");
 
-    bi.add("Create from 1-digit int", () => BigInteger.fromInt(9));
-    bi.add("Create from 16-digit int", () => BigInteger.fromInt(9007199254740991));
+    s.add("Create from 1-digit int", () => BigInteger.fromInt(9));
+    s.add("Create from 16-digit int", () => BigInteger.fromInt(9007199254740991));
 
-    bi.add("Parse 1-digit number", () => BigInteger.parse("1"));
-    bi.add("Parse 20-digit number", () => BigInteger.parse("1234578901234567890"));
-    bi.add("Parse 100-digit number", () => BigInteger.parse("12345789012345678901234578901234567890123457890123456789012345789012345678901234578901234567890"));
+    s.add("Parse 1-digit number", () => BigInteger.parse("1"));
+    s.add("Parse 20-digit number", () => BigInteger.parse("1234578901234567890"));
+    s.add("Parse 100-digit number", () => BigInteger.parse("12345789012345678901234578901234567890123457890123456789012345789012345678901234578901234567890"));
 
-    bi.add("Negate a 1-digit number", () => i1_1.negate());
-    bi.add("Negate a 20-digit number", () => i20_1.negate());
-    bi.add("Negate a 100-digit number", () => i100_1.negate());
+    s.add("Negate a 1-digit number", () => i1_1.negate());
+    s.add("Negate a 20-digit number", () => i20_1.negate());
+    s.add("Negate a 100-digit number", () => i100_1.negate());
 
-    bi.add("Absolute value of a 1-digit number", () => i1_1.abs());
-    bi.add("Absolute value of a 20-digit number", () => i20_1.abs());
-    bi.add("Absolute value of a 100-digit number", () => i100_1.abs());
+    s.add("Absolute value of a 1-digit number", () => i1_1.abs());
+    s.add("Absolute value of a 20-digit number", () => i20_1.abs());
+    s.add("Absolute value of a 100-digit number", () => i100_1.abs());
 
-    bi.add("Add two 1-digit numbers", () => i1_1.add(i1_2));
-    bi.add("Add 20-digit and 1-digit numbers", () => i20_1.add(i1_1));
-    bi.add("Add two 20-digit numbers", () => i20_1.add(i20_2));
-    bi.add("Add two 100-digit numbers", () => i100_1.add(i100_2));
+    s.add("Add two 1-digit numbers", () => i1_1.add(i1_2));
+    s.add("Add 20-digit and 1-digit numbers", () => i20_1.add(i1_1));
+    s.add("Add two 20-digit numbers", () => i20_1.add(i20_2));
+    s.add("Add two 100-digit numbers", () => i100_1.add(i100_2));
 
-    bi.add("Subtract two 1-digit numbers", () => i1_1.sub(i1_2));
-    bi.add("Subtract 20-digit and 1-digit numbers", () => i20_1.sub(i1_1));
-    bi.add("Subtract two 20-digit numbers", () => i20_1.sub(i20_2));
-    bi.add("Subtract two 100-digit numbers", () => i100_1.sub(i100_2));
+    s.add("Subtract two 1-digit numbers", () => i1_1.sub(i1_2));
+    s.add("Subtract 20-digit and 1-digit numbers", () => i20_1.sub(i1_1));
+    s.add("Subtract two 20-digit numbers", () => i20_1.sub(i20_2));
+    s.add("Subtract two 100-digit numbers", () => i100_1.sub(i100_2));
 
-    bi.add("Multiply two 1-digit numbers", () => i1_1.mul(i1_2));
-    bi.add("Multiply 20-digit and 1-digit numbers", () => i20_1.mul(i1_1));
-    bi.add("Multiply two 20-digit numbers", () => i20_1.mul(i20_2));
-    bi.add("Multiply two 100-digit numbers", () => i100_1.mul(i100_2));
+    s.add("Multiply two 1-digit numbers", () => i1_1.mul(i1_2));
+    s.add("Multiply 20-digit and 1-digit numbers", () => i20_1.mul(i1_1));
+    s.add("Multiply two 20-digit numbers", () => i20_1.mul(i20_2));
+    s.add("Multiply two 100-digit numbers", () => i100_1.mul(i100_2));
 
     // FIXME: div, mod and modInverse are so incredibly slow they're not worth benchmarking :-/
     //bi.add("Divide two 1-digit numbers", () => i1_1.div(i1_2));
@@ -99,11 +123,20 @@ requirejs([], () => {
     //bi.add("Modular inverse a 20-digit number", () => i20_1.modInverse(i20_2));
     //bi.add("Modular inverse a 100-digit number", () => i100_1.modInverse(i100_2));
 
-    bi.add("Logical AND of two 1-digit numbers", () => i1_1.and(i1_2));
-    bi.add("Logical AND of 20-digit and 1-digit numbers", () => i20_1.and(i1_1));
-    bi.add("Logical AND of two 20-digit numbers", () => i20_1.and(i20_2));
-    bi.add("Logical AND of 100-digit and 20-digit numbers", () => i100_1.and(i20_1));
-    bi.add("Logical AND of two 100-digit numbers", () => i100_1.and(i100_2));
+    s.add("Logical AND of two 1-digit numbers", () => i1_1.and(i1_2));
+    s.add("Logical AND of 20-digit and 1-digit numbers", () => i20_1.and(i1_1));
+    s.add("Logical AND of two 20-digit numbers", () => i20_1.and(i20_2));
+    s.add("Logical AND of 100-digit and 20-digit numbers", () => i100_1.and(i20_1));
+    s.add("Logical AND of two 100-digit numbers", () => i100_1.and(i100_2));
 
-    bi.run({ async: true });
+    return s;
+}
+
+function modNumberSuite(): Benchmark.Suite {
+    return createSuite("ModNumber");
+}
+
+requirejs([], () => {
+    run(bigIntegerSuite(),
+        modNumberSuite());
 });
