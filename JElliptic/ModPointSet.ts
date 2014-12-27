dd024385e7ebdd893506fca7e8536341ceac2a0e
Solal Pirelli <solal.pirelli@gmail.com>
2014-12-27 14:26:00 +0100

    ModPointSet now keeps track of the number of duplicates it gets.

--- JElliptic/ModPointSet.ts
+++ JElliptic/ModPointSet.ts
@@ -2,9 +2,17 @@
 
 class ModPointSet {
     private _points: ModPoint[];
+    private _totalCount: number;
+    private _duplicatesCount: number;
 
     constructor() {
         this._points = [];
+        this._totalCount = 0;
+        this._duplicatesCount = 0;
+    }
+
+    get uniqueFraction() {
+        return 1.0 - (this._duplicatesCount / this._totalCount);
     }
 
     contains(point: ModPoint): boolean {
@@ -17,7 +25,13 @@ class ModPointSet {
     }
 
     add(point: ModPoint): void {
-        this._points.push(point);
+        this._totalCount++;
+
+        if (this.contains(point)) {
+            this._duplicatesCount++;
+        } else {
+            this._points.push(point);
+        }
     }
 }
 
