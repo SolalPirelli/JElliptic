2d261bff117e01a411c3f222a0fd3fe252aab80a
Solal Pirelli <solal.pirelli@gmail.com>
2015-01-09 15:30:47 +0100

    Removed website stuff. See branch with_website to view it.

--- JElliptic.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JElliptic.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- JElliptic.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JElliptic.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
